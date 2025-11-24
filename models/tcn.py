644a55c72524119fa6136e8c7b0594cb99fc873d
Derek Luan <derek.t.luan@gmail.com>
2025-11-23 16:52:31 -0800

    explore rnn and tcn

--- models/tcn.py
+++ models/tcn.py
@@ -14,30 +14,32 @@ class LightCurveTCN(nn.Module):
         """
         super(LightCurveTCN, self).__init__()
         # channels = [8, 16, 32, 64, 128, 256]
-        channels = [8, 16, 32]
+        # channels = [64, 128]
+        channels = [8, 16, 32, 64]
         # channels = [16, 16, 16, 16, 16]
         # channels = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
         self.tcn = TCN(
             num_inputs=input_size,
             num_channels=channels,
-            kernel_size=2,
+            kernel_size=4,
             dilations=None,
             dropout=0.1,
             causal=False,  # For classification.
-            use_norm='weight_norm',
+            use_norm='batch_norm',
             input_shape='NLC',  # input shape is [batch, sequence length, features]
             # use_skip_connections=True,
         )
-        self.flatten = nn.Flatten()  # flatten -> (batch_size x last channel * seq length)
-        fc_input = input_size * channels[-1] * max_length
-        self.fc = nn.Linear(fc_input, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+        # self.flatten = nn.Flatten()  # flatten -> (batch_size x last channel * seq length)
+        # fc_input = input_size * channels[-1] * max_length
+        # self.fc = nn.Linear(fc_input, num_classes)
+        self.fc = nn.Linear(channels[-1], num_classes)
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
         out = self.tcn(x)
         # This step gets the last value from the sequence.
-        # out = out[:, -1, :]
-        out = self.flatten(out)
+        out = out[:, -1, :]
+        # out = self.flatten(out)
         out = self.fc(out)
         out = self.softmax(out)
         return out
